a4a16adc9b3c2fb438d9321e7a7aa754b2bb0de5
Sky9th <weitianxu@qq.com>
2018-06-05 16:23:23 +0800

    引入后台前端框架

---
+++ application/admin/config/template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sky9th
+ * Date: 2018/6/5
+ * Time: 16:00
+ */
+return [
+    'tpl_replace_string'  =>  [
+        '__STATIC__'=> __ROOT__.'/static',
+        '__ADMIN__' => __ROOT__.'/static/admin/vertical',
+    ]
+];
---
+++ application/admin/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sky9th
+ * Date: 2018/6/5
+ * Time: 13:38
+ */
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Index extends Controller{
+
+    public function index(){
+        return view();
+    }
+
+}
